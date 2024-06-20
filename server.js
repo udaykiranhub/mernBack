888b878da67c9211c1176fe440128648c7904cfd
NithiNWeb4 <lpun490@gmail.com>
2024-06-20 18:43:00 +0530

    second

---
+++ server.js
@@ -0,0 +1,50 @@
+var express=require("express");
+var app=express();
+
+////
+var bodyparser=require("body-parser");
+var cors=require("cors");
+var mongoose=require("mongoose");
+require("dotenv").config();
+//
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+///
+const corsOptions = {
+    origin: ['http://localhost:3000',"https://your-frontend-url.onrender.com"],
+    optionsSuccessStatus: 200, // For legacy browser support
+  };
+  
+  app.use(cors(corsOptions));
+
+///////////////////Database connecting
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(function(){
+    console.log("Database connected Successfully!");
+})
+.catch(function(err){
+    console.log("Error in the Database connection!" + err);
+});
+
+
+////////////////////////////
+
+
+//routers.......
+
+var Register=require("./routers/registerrouter");
+
+
+//
+
+app.use("/",Register);
+
+app.listen(5000,function(err){
+    if(err){
+        console.log("Error in the server Ruinning!");
+    }
+    else{
+        console.log("Server is Running!");
+
+    }
+})
